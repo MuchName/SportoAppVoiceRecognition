ab93bc47bed80b6fa501d4e499ec48e751100f7d
Marius <35076113+MuchName@users.noreply.github.com>
2019-07-04 21:08:18 +0300

    commit

---
+++ SportoAppVR/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace SportoAppVR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblSekundes = new System.Windows.Forms.Label();
+            this.lblKartai = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblSekundes
+            // 
+            this.lblSekundes.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.lblSekundes.Location = new System.Drawing.Point(370, 95);
+            this.lblSekundes.Name = "lblSekundes";
+            this.lblSekundes.Size = new System.Drawing.Size(195, 76);
+            this.lblSekundes.TabIndex = 2;
+            this.lblSekundes.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblKartai
+            // 
+            this.lblKartai.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.lblKartai.Location = new System.Drawing.Point(370, 202);
+            this.lblKartai.Name = "lblKartai";
+            this.lblKartai.Size = new System.Drawing.Size(195, 76);
+            this.lblKartai.TabIndex = 2;
+            this.lblKartai.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label1.Location = new System.Drawing.Point(29, 202);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(281, 76);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kartai";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label2.Location = new System.Drawing.Point(12, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(325, 76);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Sekundės";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblKartai);
+            this.Controls.Add(this.lblSekundes);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblSekundes;
+        private System.Windows.Forms.Label lblKartai;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SportoAppVR/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Media;
+
+
+namespace SportoAppVR
+{    
+    
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
+        //SoundPlayer player = new SoundPlayer("C:/Users/Marius/Downloads/beep.wav");
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void Play()
+        {
+            string soundfile = @"C:/Users/Marius/Downloads/warningbeep.wav";
+            var sound = new SoundPlayer(soundfile);
+            sound.Play();
+        }
+
+        int sekundes = 1;
+        int kartai = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblSekundes.Text =""+ sekundes++;
+
+            if(sekundes == 11)
+            {
+                lblSekundes.Text = "0";
+                lblKartai.Text = ""+ ++kartai;
+                Play();
+                sekundes = 1;
+                
+            }
+            if (kartai >= 20)
+            {
+                timer1.Enabled = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Choices commands = new Choices();
+            commands.Add(new string[] { "start", "stop", "reset"});
+            GrammarBuilder gBuilder = new GrammarBuilder();
+            gBuilder.Append(commands);
+            Grammar grammar = new Grammar(gBuilder);
+
+            recEngine.LoadGrammarAsync(grammar);
+            recEngine.SetInputToDefaultAudioDevice();
+            recEngine.SpeechRecognized += recEngine_SpeechRecognized;
+            recEngine.RecognizeAsync(RecognizeMode.Multiple);
+        }
+
+        void recEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch(e.Result.Text)
+            {
+                case "start":
+                    Play();
+                    //MessageBox.Show("Hello, Marius. How are you?");
+
+                    timer1.Enabled = true;
+
+                    break;
+                case "stop":
+                    timer1.Enabled = false;
+                    break;
+                case "reset":
+                    lblSekundes.Text = "0";
+                    lblKartai.Text = "0";
+                    kartai = 0;
+                    sekundes = 1;
+                    break;
+                
+                  
+            }
+        }
+
+        private void btnEnable_Click(object sender, EventArgs e)
+        {
+           // Play();
+        }
+
+        private void btnDisable_Click(object sender, EventArgs e)
+        {
+            //recEngine.RecognizeAsyncStop();
+            //btnDisable.Enabled = false;
+        }
+    }
+}
